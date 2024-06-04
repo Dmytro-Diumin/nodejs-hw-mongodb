f95fa9d0dafd486d18d492a6edffd2c81e9a1137
Dmytro-Diumin <microb201196@gmail.com>
2024-06-04 23:01:20 +0300

    added contacts.json fix router & controllers

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,14 +1,32 @@
-import { getAllContactsService } from '../services/contacts.js';
+import Contact from '../models/contact.js';
 
 export const getAllContacts = async (req, res) => {
   try {
-    const contacts = await getAllContactsService();
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
+
+// Новий контролер для отримання контакту за ID
+export const getContactById = async (req, res) => {
+  const { contactId } = req.params;
+
+  try {
+    const contact = await Contact.findById(contactId);
+
+    if (!contact) {
+      return res
+        .status(404)
+        .json({ message: `Contact with id ${contactId} not found` });
+    }
+
     res.status(200).json({
-      status: 'success',
-      message: 'Successfully found contacts!',
-      data: contacts,
+      message: `Successfully found contact with id ${contactId}!`,
+      contact,
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: 'Server error', error });
   }
 };
